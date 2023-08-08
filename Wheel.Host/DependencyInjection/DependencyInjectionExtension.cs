bf3a54b052666f341b84e9ad8b17e19a2cf61de3
funshow <136590076@qq.com>
2023-08-08 17:43:29 +0800

    update

--- Wheel.Host/DependencyInjection/DependencyInjectionExtension.cs
+++ Wheel.Host/DependencyInjection/DependencyInjectionExtension.cs
@@ -9,64 +9,62 @@ namespace Wheel.DependencyInjection
     {
         public static IServiceCollection InitWheelDependency(this IServiceCollection services)
         {
-            var abs = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in abs)
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(ab=>ab.GetTypes());
+            var transientTypes = types
+                .Where(t => !t.IsInterface)
+                .Where(t => t.GetInterface(nameof(ITransientDependency))!= null);
+            foreach (var transientType in transientTypes)
             {
-                var transientTypes = assembly.GetTypes()
-                    .Where(t => !t.IsInterface)
-                    .Where(t => t.GetInterface(nameof(ITransientDependency))!= null);
-                foreach (var transientType in transientTypes)
+                var typeInterfaces = transientType.GetInterfaces();
+                if (typeInterfaces.Length > 0)
                 {
-                    var typeInterfaces = transientType.GetInterfaces();
-                    if (typeInterfaces.Length > 0)
+                    foreach (var typeInterface in typeInterfaces)
                     {
-                        foreach (var typeInterface in typeInterfaces)
-                        {
-                            services.AddSingleton(typeInterface, transientType);
-                        }
-                    }
-                    else
-                    {
-                        services.AddTransient(transientType);
+                        services.AddSingleton(typeInterface, transientType);
                     }
                 }
-                var scopeTypes = assembly.GetTypes()
-                    .Where(t => !t.IsInterface)
-                    .Where(t => t.GetInterface(nameof(IScopeDependency))!= null);
-                foreach (var scopeType in scopeTypes)
+                else
                 {
-                    var typeInterfaces = scopeType.GetInterfaces();
-                    if (typeInterfaces.Length > 0)
-                    {
-                        foreach (var typeInterface in typeInterfaces)
-                        {
-                            services.AddSingleton(typeInterface, scopeType);
-                        }
-                    }
-                    else
-                    {
-                        services.AddScoped(scopeType);
-                    }
-                }
-                var singletonTypes = assembly.GetTypes()
-                    .Where(t => !t.IsInterface)
-                    .Where(t => t.GetInterface(nameof(ISingletonDependency))!= null);
-                foreach (var singletonType in singletonTypes)
-                {
-                    var typeInterfaces = singletonType.GetInterfaces();
-                    if (typeInterfaces.Length > 0)
-                    {
-                        foreach (var typeInterface in typeInterfaces)
-                        {
-                            services.AddSingleton(typeInterface, singletonType);
-                        }
-                    }
-                    else
-                    {
-                        services.AddSingleton(singletonType);
-                    }
+                    services.AddTransient(transientType);
                 }
             }
+            var scopeTypes = types
+                .Where(t => !t.IsInterface)
+                .Where(t => t.GetInterface(nameof(IScopeDependency))!= null);
+            foreach (var scopeType in scopeTypes)
+            {
+                var typeInterfaces = scopeType.GetInterfaces();
+                if (typeInterfaces.Length > 0)
+                {
+                    foreach (var typeInterface in typeInterfaces)
+                    {
+                        services.AddSingleton(typeInterface, scopeType);
+                    }
+                }
+                else
+                {
+                    services.AddScoped(scopeType);
+                }
+            }
+            var singletonTypes = types
+                .Where(t => !t.IsInterface)
+                .Where(t => t.GetInterface(nameof(ISingletonDependency))!= null);
+            foreach (var singletonType in singletonTypes)
+            {
+                var typeInterfaces = singletonType.GetInterfaces();
+                if (typeInterfaces.Length > 0)
+                {
+                    foreach (var typeInterface in typeInterfaces)
+                    {
+                        services.AddSingleton(typeInterface, singletonType);
+                    }
+                }
+                else
+                {
+                    services.AddSingleton(singletonType);
+                }
+            }
+            
             return services;
         }
     }
