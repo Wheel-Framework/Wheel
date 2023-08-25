7f225658d276d39c4b67ab06dad65b6b47e97b44
funshow <136590076@qq.com>
2023-08-25 18:26:23 +0800

    Update

---
+++ backend/Wheel.WebApi.Host/Const/ErrorCode.cs
@@ -0,0 +1,13 @@
+﻿namespace Wheel.Const
+{
+    /// <summary>
+    /// 错误码
+    /// 约定5位数字字符串
+    /// 4XXXX：客户端错误信息
+    /// 5XXXX: 服务端错误信息
+    /// </summary>
+    public class ErrorCode
+    {
+        
+    }
+}
