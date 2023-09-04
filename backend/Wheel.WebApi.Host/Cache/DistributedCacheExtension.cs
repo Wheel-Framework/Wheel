4382bbd5ca83f73b0170eedec0e28acb37d7874c
funshow <136590076@qq.com>
2023-09-04 17:02:27 +0800

    Update

---
+++ backend/Wheel.WebApi.Host/Cache/DistributedCacheExtension.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+
+namespace Microsoft.Extensions.Caching.Distributed
+{
+    public static class DistributedCacheExtension
+    {
+        public static async Task<T> GetAsync<T>(this IDistributedCache cache, string key, CancellationToken cancellationToken = default)
+        {
+            var value = await cache.GetStringAsync(key, cancellationToken);
+            if (string.IsNullOrWhiteSpace(value))
+                return default(T);
+            return JsonSerializer.Deserialize<T>(value);
+        }
+        public static async Task SetAsync<T>(this IDistributedCache cache, string key, T value, CancellationToken cancellationToken = default)
+        {
+            await cache.SetStringAsync(key, JsonSerializer.Serialize(value), cancellationToken);
+        }
+        public static async Task SetAsync<T>(this IDistributedCache cache, string key, T value, DistributedCacheEntryOptions distributedCacheEntryOptions, CancellationToken cancellationToken = default)
+        {
+            await cache.SetStringAsync(key, JsonSerializer.Serialize(value), distributedCacheEntryOptions, cancellationToken);
+        }
+        public static async Task SetAbsoluteExpirationRelativeToNowAsync<T>(this IDistributedCache cache, string key, T value, TimeSpan timeSpan, CancellationToken cancellationToken = default)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = timeSpan
+            };
+            await cache.SetStringAsync(key, JsonSerializer.Serialize(value), options, cancellationToken);
+        }
+        public static async Task SetAbsoluteExpirationAsync<T>(this IDistributedCache cache, string key, T value, DateTimeOffset dateTimeOffset, CancellationToken cancellationToken = default)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = dateTimeOffset
+            };
+            await cache.SetStringAsync(key, JsonSerializer.Serialize(value), options, cancellationToken);
+        }
+        public static async Task SetSlidingExpirationAsync<T>(this IDistributedCache cache, string key, T value, TimeSpan slidingExpiration, CancellationToken cancellationToken = default)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                SlidingExpiration = slidingExpiration
+            };
+            await cache.SetStringAsync(key, JsonSerializer.Serialize(value), options, cancellationToken);
+        }
+    }
+}
--- backend/Wheel.WebApi.Host/Services/PermissionManage/PermissionManageAppService.cs
+++ backend/Wheel.WebApi.Host/Services/PermissionManage/PermissionManageAppService.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Caching.Distributed;
 using System.Reflection;
+using System.Text.Json;
 using System.Xml;
 using Wheel.DependencyInjection;
 using Wheel.Domain;
@@ -19,41 +21,46 @@ namespace Wheel.Services.PermissionManage
         {
             _xmlCommentHelper = xmlCommentHelper;
         }
-        public Task<List<GetAllPermissionDto>> GetAllPermission()
+        public async Task<List<GetAllPermissionDto>> GetAllPermission()
         {
-            var result = new List<GetAllPermissionDto>();
-            var apiDescriptionGroupCollectionProvider = ServiceProvider.GetRequiredService<IApiDescriptionGroupCollectionProvider>();
-            var apiDescriptionGroups = apiDescriptionGroupCollectionProvider.ApiDescriptionGroups.Items.SelectMany(group => group.Items)
-                .Where(a => a.ActionDescriptor is ControllerActionDescriptor)
-                .GroupBy(a => (a.ActionDescriptor as ControllerActionDescriptor).ControllerTypeInfo);
-
-            foreach (var apiDescriptions in apiDescriptionGroups)
+            var result = await DistributedCache.GetAsync<List<GetAllPermissionDto>>("AllDefinePermission");
+            if(result == null)
             {
-                var permissionGroup = new GetAllPermissionDto();
-                var controllerTypeInfo = apiDescriptions.Key;
+                result = new List<GetAllPermissionDto>();
+                var apiDescriptionGroupCollectionProvider = ServiceProvider.GetRequiredService<IApiDescriptionGroupCollectionProvider>();
+                var apiDescriptionGroups = apiDescriptionGroupCollectionProvider.ApiDescriptionGroups.Items.SelectMany(group => group.Items)
+                    .Where(a => a.ActionDescriptor is ControllerActionDescriptor)
+                    .GroupBy(a => (a.ActionDescriptor as ControllerActionDescriptor).ControllerTypeInfo);
 
-                var controllerAllowAnonymous = controllerTypeInfo.GetAttribute<AllowAnonymousAttribute>();
-
-                var controllerComment = _xmlCommentHelper.GetTypeComment(controllerTypeInfo);
-
-                permissionGroup.Group = controllerTypeInfo.Name;
-                permissionGroup.Summary = controllerComment;
-                foreach (var apiDescription in apiDescriptions)
+                foreach (var apiDescriptions in apiDescriptionGroups)
                 {
-                    var method = controllerTypeInfo.GetMethod(apiDescription.ActionDescriptor.RouteValues["action"]);
-                    var actionAllowAnonymous = method.GetAttribute<AllowAnonymousAttribute>();
-                    var actionAuthorize = method.GetAttribute<AuthorizeAttribute>();
-                    if((controllerAllowAnonymous == null && actionAllowAnonymous == null) || actionAuthorize != null)
+                    var permissionGroup = new GetAllPermissionDto();
+                    var controllerTypeInfo = apiDescriptions.Key;
+
+                    var controllerAllowAnonymous = controllerTypeInfo.GetAttribute<AllowAnonymousAttribute>();
+
+                    var controllerComment = _xmlCommentHelper.GetTypeComment(controllerTypeInfo);
+
+                    permissionGroup.Group = controllerTypeInfo.Name;
+                    permissionGroup.Summary = controllerComment;
+                    foreach (var apiDescription in apiDescriptions)
                     {
-                        var methodComment = _xmlCommentHelper.GetMethodComment(method);
-                        permissionGroup.Permissions.Add(new PermissionDto { Name = method.Name, Summary = methodComment });
+                        var method = controllerTypeInfo.GetMethod(apiDescription.ActionDescriptor.RouteValues["action"]);
+                        var actionAllowAnonymous = method.GetAttribute<AllowAnonymousAttribute>();
+                        var actionAuthorize = method.GetAttribute<AuthorizeAttribute>();
+                        if ((controllerAllowAnonymous == null && actionAllowAnonymous == null) || actionAuthorize != null)
+                        {
+                            var methodComment = _xmlCommentHelper.GetMethodComment(method);
+                            permissionGroup.Permissions.Add(new PermissionDto { Name = method.Name, Summary = methodComment });
+                        }
                     }
+                    if (permissionGroup.Permissions.Count > 0)
+                        result.Add(permissionGroup);
                 }
-                if(permissionGroup.Permissions.Count > 0)
-                    result.Add(permissionGroup);
+                await DistributedCache.SetAsync("AllDefinePermission", result);
             }
             // todo: 获取当前用户角色已有权限并标记
-            return Task.FromResult(result);
+            return result;
         }
     }
 }
