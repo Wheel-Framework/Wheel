c05af1b5942add32b5433395b5f6fb6493f06584
able <2141576@qq.com>
2023-09-13 23:48:39 +0800

    提交代码

--- backend/Wheel.WebApi.Host/Program.cs
+++ backend/Wheel.WebApi.Host/Program.cs
@@ -59,7 +59,7 @@ builder.Services.AddAuthentication(IdentityConstants.BearerScheme)
             {
                 var accessToken = context.Request.Query["access_token"];
                 // If the request is for our hub...
-                var path = context.HttpContext.Request.Path;
+                var path = context.HttpContext.Request.Path; 
                 if (!string.IsNullOrEmpty(accessToken) &&
                     (path.StartsWithSegments("/hubs")))
                 {
