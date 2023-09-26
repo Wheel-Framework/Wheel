0a6113f6878baa072df47bb5dbe0b625cd335a6e
funshow <136590076@qq.com>
2023-09-26 15:59:06 +0800

    update

--- frontend/WheelApp/src/access.ts
+++ frontend/WheelApp/src/access.ts
@@ -14,7 +14,6 @@ export default function access(initialState: { currentUser?: API.ICurrentUser, p
   };
   return {
     canAdmin: currentUser && currentUser.roles?.indexOf('admin') !== -1,
-    abc: false,
     isGranted
   };
 }
