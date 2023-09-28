5e708ddc79393b40898031a0c673a479d2d3a427
funshow <136590076@qq.com>
2023-09-28 16:15:02 +0800

    菜单管理CURD

--- frontend/WheelApp/src/pages/System/Menu/index.tsx
+++ frontend/WheelApp/src/pages/System/Menu/index.tsx
@@ -1,35 +1,45 @@
-import { DownOutlined, EditOutlined, PlusCircleOutlined, PlusOutlined, RedoOutlined } from '@ant-design/icons';
-import type { ActionType, ProColumns } from '@ant-design/pro-components';
-import { ProTable } from '@ant-design/pro-components';
+import { EditOutlined, PlusOutlined, RedoOutlined } from '@ant-design/icons';
+import type { ActionType, ProColumns, ProFormColumnsType, ProFormLayoutType } from '@ant-design/pro-components';
+import { ProTable, BetaSchemaForm, ProFormSelect } from '@ant-design/pro-components';
 import { Button, Tag } from 'antd';
 import { L } from '@/Utilities/Localization';
-import { getMenu } from '@/services/Wheel/Menu';
-import menu from '@/locales/bn-BD/menu';
-import { useRef } from 'react';
-
+import { getMenu, postMenu,deleteMenuId, putMenuId } from '@/services/Wheel/Menu';
+import { useRef, useState } from 'react';
+let parents : any = []
 const columns: ProColumns<API.MenuDto>[] = [
   {
     title: 'Id',
     width: 120,
     dataIndex: 'id',
     hideInTable: true,
+    hideInForm: true,
     valueType: 'text',
   },
   {
+    title: L('parentId'),
+    width: 240,
+    dataIndex: 'parentId',
+    hideInTable: true,
+    request: async () => {
+      return parents
+    }
+  },
+  {
     title: L('name'),
-    width: 120,
+    width: 240,
     dataIndex: 'name',
     valueType: 'text',
   },
   {
     title: L('displayName'),
-    width: 120,
+    width: 240,
     dataIndex: 'displayName',
   },
   {
     title: L('menuType'),
-    width: 120,
+    width: 240,
     dataIndex: 'menuType',
+    valueType: "select",
     valueEnum: {
       0: { text: L('MenuType.Menu', '菜单') },
       1: { text: L('MenuType.Page', '页面') },
@@ -38,21 +48,23 @@ const columns: ProColumns<API.MenuDto>[] = [
   },
   {
     title: L('path'),
-    width: 120,
+    width: 240,
     dataIndex: 'path',
   },
   {
     title: 'icon',
-    width: 120,
+    width: 240,
     dataIndex: 'icon',
   },
   {
     title: L('sort'),
     width: 120,
     dataIndex: 'sort',
+    valueType: 'digit'
   },
   {
     title: L('operate'),
+    width: 120,
     valueType: 'option',
     key: 'option',
     render: (text, record, _, action) => [
@@ -69,6 +81,7 @@ const columns: ProColumns<API.MenuDto>[] = [
 ];
 
 const Menu: React.FC = () => {
+  const [layoutType] = useState<ProFormLayoutType>('ModalForm');
 
   const ref = useRef<ActionType>();
   const reload = () => {
@@ -77,9 +90,12 @@ const Menu: React.FC = () => {
   return (
     <ProTable<API.MenuDto>
       columns={columns}
-      request={async (params, sorter, filter) => {
+      request={async () => {
         // 表单搜索项会从 params 传入，传递给后端接口。
         const menuData = await getMenu();
+        parents = menuData.data?.map((val) => {
+          return { 'label': val.name, 'value': val.id } as any
+        }) || []
         return Promise.resolve({
           data: menuData.data,
           success: true,
@@ -91,11 +107,44 @@ const Menu: React.FC = () => {
       dateFormatter="string"
       headerTitle={L('menu.SystemManage.MenuManage')}
       options={false}
+      editable={{
+        onDelete: async (rowKey) =>{
+          await deleteMenuId({id: rowKey as string})
+          reload()
+        },
+        onSave: async (rowKey, data) => {
+          data.children = undefined
+          await putMenuId({id: rowKey as string}, data as API.CreateOrUpdateMenuDto)
+          reload()
+        },
+      }}
       actionRef={ref}
       toolBarRender={() => [
-        <Button key="add" type="default">
-          <PlusOutlined />{L('add')}
-        </Button>,
+        <BetaSchemaForm<API.MenuDto>
+          key="add"
+          trigger={<Button type="default"><PlusOutlined />{L('add')}</Button>}
+          layoutType={layoutType}
+          rowProps={{
+            gutter: [16, 16],
+          }}
+          colProps={{
+            span: 12,
+          }}
+          shouldUpdate={false}
+          grid={true}
+          submitTimeout={2000}
+          onFinish={async (values) => {
+            values.menuType = Number(values.menuType) as API.MenuType
+            await postMenu(values as API.CreateOrUpdateMenuDto )
+            reload()
+            return true
+          }}
+          {...{
+            modalProps: { destroyOnClose: true }
+          }}
+          columns={columns as any}
+        />
+        ,
         <Button key="refresh" type="primary" onClick={reload}>
           <RedoOutlined /> {L('refresh')}
         </Button>,
