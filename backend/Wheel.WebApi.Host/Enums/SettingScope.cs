51cb7da7f84066f0e33b631ea2b4193d685ac2ef
funshow <136590076@qq.com>
2023-10-18 17:21:31 +0800

    Update

--- backend/Wheel.WebApi.Host/Enums/SettingScope.cs
+++ backend/Wheel.WebApi.Host/Enums/SettingScope.cs
@@ -7,10 +7,6 @@
         /// </summary>
         Golbal,
         /// <summary>
-        /// 角色设置
-        /// </summary>
-        Role,
-        /// <summary>
         /// 用户设置
         /// </summary>
         User,
