6ef708c528f573feed9eaec3a36ccc8ce741b4c9
funshow <136590076@qq.com>
2023-10-23 10:02:41 +0800

    Update

--- backend/Wheel.WebApi.Host/Domain/FileStorages/FileStorageTypeChecker.cs
+++ backend/Wheel.WebApi.Host/Domain/FileStorages/FileStorageTypeChecker.cs
@@ -6,16 +6,14 @@ namespace Wheel.Domain.FileStorages
     {
         public static FileStorageType CheckFileType(string contentType)
         {
-            if (contentType.StartsWith("audio"))
-                return FileStorageType.Audio;
-            if (contentType.StartsWith("image"))
-                return FileStorageType.Image;
-            if (contentType.StartsWith("text"))
-                return FileStorageType.Text;
-            if (contentType.StartsWith("video"))
-                return FileStorageType.Video;
-
-            return FileStorageType.File;
+            return contentType switch
+            {
+                var _ when contentType.StartsWith("audio") => FileStorageType.Audio,
+                var _ when contentType.StartsWith("image") => FileStorageType.Image,
+                var _ when contentType.StartsWith("text") => FileStorageType.Text,
+                var _ when contentType.StartsWith("video") => FileStorageType.Video,
+                _ => FileStorageType.File
+            };
         }
     }
 }
